a785905a3c53b36688ba50484177b6b9146cad98
Xvclnn <shinshinpachi1030@gmail.com>
2026-04-27 23:39:44 +0800

    from da scratch

--- src/itasksys.h
+++ src/itasksys.h
@@ -1,16 +1,7 @@
 #pragma once
 
-// Node for singly linked list
-struct Node {
-    int val;
-    Node* next;
-    explicit Node(int v) : val(v), next(nullptr) {}
-};
-
-// Abstract interface for all sort variants
-class ITaskSystem {
+class TaskSystem {
 public:
-    // Sort a singly linked list in ascending order, return new head
-    virtual Node* sort(Node* head) = 0;
-    virtual ~ITaskSystem() {}
+    virtual void run_sort(int num_threads, float* array, int array_size) = 0;
+    virtual ~TaskSystem() = default;
 };
--- src/tasksys.h
+++ src/tasksys.h
@@ -1,43 +1,26 @@
 #pragma once
 #include "itasksys.h"
 
-// -------------------------------------------------------
-// 1. Sequential – single-threaded merge sort
-// -------------------------------------------------------
-class TaskSystemSerial : public ITaskSystem {
+void reset_array(const float* base_array, float* sorting_array, int array_size);
+
+class TaskSystemSerial : public TaskSystem {
 public:
-    Node* sort(Node* head) override;
+    void run_sort(int num_threads, float* array, int array_size) override;
 };
 
-// -------------------------------------------------------
-// 2. Multithreaded CPU – std::thread
-//    Spawns threads up to log2(hardware_concurrency) deep
-// -------------------------------------------------------
-class TaskSystemThread : public ITaskSystem {
-    int threadDepth;
+class TaskSystemThread : public TaskSystem {
 public:
-    TaskSystemThread();
-    Node* sort(Node* head) override;
+    void run_sort(int num_threads, float* array, int array_size) override;
 };
 
-// -------------------------------------------------------
-// 3. Parallel CPU – OpenMP tasks
-//    Uses #pragma omp task for recursive parallelism
-// -------------------------------------------------------
-class TaskSystemOpenMP : public ITaskSystem {
-    int taskDepth;
+class TaskSystemOpenMP : public TaskSystem {
 public:
-    TaskSystemOpenMP();
-    Node* sort(Node* head) override;
+    void run_sort(int num_threads, float* array, int array_size) override;
 };
 
-// -------------------------------------------------------
-// 4. GPU – CUDA (compiled separately with nvcc)
-//    Linearises list -> device sort -> reconstructs list
-// -------------------------------------------------------
 #ifdef USE_CUDA
-class TaskSystemCUDA : public ITaskSystem {
+class TaskSystemCUDA : public TaskSystem {
 public:
-    Node* sort(Node* head) override;
+    void run_sort(int num_threads, float* array, int array_size) override;
 };
 #endif
